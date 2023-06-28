7a203c59abee4b2ccd047635106ad9b1a4e5cd4d
Wojtek Opulski <wojtekopulski@gmail.com>
2023-06-29 00:07:12 +0200

    feat: quoteBlock component

--- src/components/Atoms/RichText.tsx
+++ src/components/Atoms/RichText.tsx
@@ -1,15 +1,15 @@
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 
+import BlockQuote from './BlockQuote';
+
 type RichTextProps = {
   data: string;
 };
 
 const RichText = ({ data }: RichTextProps) => (
   <section className="rich-text py-2">
-    <Markdown className="prose lg:prose-xl" remarkPlugins={[remarkGfm]}>
-      {data}
-    </Markdown>
+    <Markdown remarkPlugins={[remarkGfm]}>{data}</Markdown>
   </section>
 );
 
