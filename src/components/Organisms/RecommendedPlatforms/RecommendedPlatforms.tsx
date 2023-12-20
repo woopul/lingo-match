65c8a99978fe36a8128f23d059560de40cf59c38
Wojtek Opulski <wojtekopulski@gmail.com>
2023-12-21 00:19:02 +0100

    feat: recommended cards carousel, platform page adjusted

--- src/components/Organisms/RecommendedPlatforms/RecommendedPlatforms.tsx
+++ src/components/Organisms/RecommendedPlatforms/RecommendedPlatforms.tsx
@@ -1,9 +1,13 @@
 import { CurrencyResponseType } from '@lingo-match/api/currency';
 import { Image, Link } from '@lingo-match/components/Atoms';
+import { MD } from '@lingo-match/constants/mediaQueries';
 import { formatPrice } from '@lingo-match/helpers/formatPrice';
+import { useMediaQuery } from '@lingo-match/hooks/useMediaQuery';
 import { BlockType, PlatformDTOMapToRecommendedCard } from '@lingo-match/types/strapi';
 import { getPlatformUrl } from '@lingo-match/utlis';
 import { cn } from '@lingo-match/utlis/cn';
+import { Grid, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 export type RecommendedPlatformsLabelsType = {
   descriptionUnderPriceLabel?: string;
@@ -27,67 +31,76 @@ const RecommendedPlatforms = ({
   labels,
   platforms,
   title,
-}: RecommendedPlatformsBlockType) => (
-  <div className={className}>
-    <h3 className="text-center">{title}</h3>
-    <div className={cn('mt-[32px] grid grid-cols-2 gap-[20px] md:flex', cardWrapperClassName)}>
-      {platforms?.map((item, i) => {
-        const { currency, mainCurrencyForThisMarket, priceAsNumber } = item;
-        const isForeignCurrency = () => currency !== mainCurrencyForThisMarket;
+}: RecommendedPlatformsBlockType) => {
+  const isDesktop = useMediaQuery(MD);
+  const slidesPerView = isDesktop ? 4.5 : 2.1;
 
-        // TODO - change it to use mixed currencies pair exchange rate
-        const getCalculatedValueInPLN = (price: number) => {
-          const currencyRate =
-            currenciesExchangeRate?.find((item) => item.code === currency)?.mid ?? 1;
-          return price * currencyRate;
-        };
+  return (
+    <div className={className}>
+      <h3 className="text-center">{title}</h3>
+      <div className={cn('mt-[32px] md:flex', '[&>.swiper]:-mx-[15px] [&>.swiper]:px-[10px]')}>
+        <Swiper slidesPerView={isDesktop ? 4.5 : 2.1} spaceBetween={isDesktop ? 16 : 8}>
+          {platforms?.map((item, i) => {
+            const { currency, mainCurrencyForThisMarket, priceAsNumber } = item;
+            const isForeignCurrency = () => currency !== mainCurrencyForThisMarket;
 
-        const parseAndFormatPriceToCorrectCurrency = (price: number) => {
-          let priceValue = price;
-          if (isForeignCurrency()) {
-            priceValue = getCalculatedValueInPLN(price);
-          }
-          return formatPrice(priceValue);
-        };
+            // TODO - change it to use mixed currencies pair exchange rate
+            const getCalculatedValueInPLN = (price: number) => {
+              const currencyRate =
+                currenciesExchangeRate?.find((item) => item.code === currency)?.mid ?? 1;
+              return price * currencyRate;
+            };
 
-        return (
-          <Link
-            className="flex aspect-[7/9] w-full flex-col items-center gap-2 rounded-md px-[8px] py-[16px] shadow-lg"
-            href={getPlatformUrl(item.slug)}
-            key={i}
-          >
-            <div className="relative h-[40px] w-full">
-              <Image alt="image" src={item.logo?.data.attributes.url} />
-            </div>
-            <h4>{item.title}</h4>
-            <ul className="flex w-full flex-col items-center rounded-lg bg-lighterGrey py-1">
-              {item.labels.data?.slice(0, 3).map((label, i) => (
-                <li
-                  className="text-small before: font-semibold text-darkGrey before:mx-auto before:my-[3px] before:block before:h-[3px] before:w-[3px] before:rounded-full before:bg-darkGrey first:before:hidden"
-                  key={i}
+            const parseAndFormatPriceToCorrectCurrency = (price: number) => {
+              let priceValue = price;
+              if (isForeignCurrency()) {
+                priceValue = getCalculatedValueInPLN(price);
+              }
+              return formatPrice(priceValue);
+            };
+
+            return (
+              <SwiperSlide key={item.slug}>
+                <Link
+                  className="my-[5%] flex aspect-[7/9] w-full flex-col items-center gap-2 rounded-md px-[8px] py-[16px] shadow-lg"
+                  href={getPlatformUrl(item.slug)}
                 >
-                  {label.attributes.title}
-                </li>
-              ))}
-            </ul>
-            <div className="mt-auto self-end text-darkGrey">
-              <div className="text-preamble whitespace-nowrap text-right font-semibold text-black">
-                {parseAndFormatPriceToCorrectCurrency(priceAsNumber)} {mainCurrencyForThisMarket}
-                <span className="text-xsmall font-normal text-darkGrey">
-                  {' '}
-                  {labels.priceSlashLabel}
-                </span>
-              </div>
-              <p className="text-small">{labels.descriptionUnderPriceLabel}</p>
-              <p className="mt-0 h-[12px] text-right text-10">
-                {isForeignCurrency() ? labels.foreignCurrencyDescriptionLabel : ' '}
-              </p>
-            </div>
-          </Link>
-        );
-      })}
+                  <div className="relative h-[40px] w-full">
+                    <Image alt="image" src={item.logo?.data.attributes.url} />
+                  </div>
+                  <h4>{item.title}</h4>
+                  <ul className="flex w-full flex-col items-center rounded-lg bg-lighterGrey py-1">
+                    {item.labels.data?.slice(0, 3).map((label, i) => (
+                      <li
+                        className="text-small before: font-semibold text-darkGrey before:mx-auto before:my-[3px] before:block before:h-[3px] before:w-[3px] before:rounded-full before:bg-darkGrey first:before:hidden"
+                        key={i}
+                      >
+                        {label.attributes.title}
+                      </li>
+                    ))}
+                  </ul>
+                  <div className="mt-auto self-end text-darkGrey">
+                    <div className="text-preamble whitespace-nowrap text-right font-semibold text-black">
+                      {parseAndFormatPriceToCorrectCurrency(priceAsNumber)}{' '}
+                      {mainCurrencyForThisMarket}
+                      <span className="text-xsmall font-normal text-darkGrey">
+                        {' '}
+                        {labels.priceSlashLabel}
+                      </span>
+                    </div>
+                    <p className="text-small">{labels.descriptionUnderPriceLabel}</p>
+                    <p className="mt-0 h-[12px] text-right text-10">
+                      {isForeignCurrency() ? labels.foreignCurrencyDescriptionLabel : ' '}
+                    </p>
+                  </div>
+                </Link>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default RecommendedPlatforms;
---
+++ src/constants/mediaQueries.ts
@@ -0,0 +1,2 @@
+export const MD = '(min-width: 768px)';
+export const LG = '(min-width: 1024px)';
