93f4f909cec55667419a81c7fe227041b7969e0e
Wojtek Opulski <wojtekopulski@gmail.com>
2024-01-07 00:30:58 +0100

    fix: route to home from design system

--- src/components/Atoms/HeaderPlaceholder.tsx
+++ src/components/Atoms/HeaderPlaceholder.tsx
@@ -1,12 +1,13 @@
+import { HOME_PAGE_ROUTE } from '@lingo-match/constants/routes';
 import Link from 'next/link';
 
 const HeaderPlaceholder = () => (
   <div className="sticky top-0 z-10 flex w-full items-center justify-between bg-inherit px-5 py-2 shadow-2xl backdrop-blur-xl">
-    <Link className="no-underline" href="/">
+    <Link className="no-underline" href={HOME_PAGE_ROUTE}>
       <h1 className="text-h2 font-bold ">LingoMatch</h1>
     </Link>
     <nav className="flex gap-2">
-      <Link className="styled-link" href="/">
+      <Link className="styled-link" href={HOME_PAGE_ROUTE}>
         <div className="text-preamble">home</div>
       </Link>
       <Link className="styled-link" href="/design-system">
