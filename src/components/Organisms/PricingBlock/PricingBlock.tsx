6d0ae59a526ef83a7a66a629a707c504524defdc
Wojtek Opulski <wojtekopulski@gmail.com>
2024-01-07 20:28:29 +0100

    feat: improve pricing block sticky againgst dynamic header value

--- src/components/Organisms/PricingBlock/PricingBlock.tsx
+++ src/components/Organisms/PricingBlock/PricingBlock.tsx
@@ -5,6 +5,7 @@ import { formatPrice } from '@lingo-match/helpers/formatPrice';
 import { pricingBlockMock } from '@lingo-match/mocks/pricingBlock';
 import { PricingBlockDTO, SubscriptionTypeDTO } from '@lingo-match/types/strapi';
 import { cn } from '@lingo-match/utlis/cn';
+import { useEffect, useState } from 'react';
 import { IoPricetagsOutline } from 'react-icons/io5';
 
 import { CTAButton } from './components/CTAButton';
@@ -34,6 +35,12 @@ export const PricingBlock = ({
   subscriptionType,
 }: PriceBlockProps) => {
   const pricingBlockLabels = useLabels('pricingBlock') as PricingBlockStrapiLabels;
+  const [headerHeight, setHeaderHeight] = useState(0);
+
+  useEffect(() => {
+    const headerHeight = document.getElementById('header')?.offsetHeight || 0;
+    setHeaderHeight(headerHeight);
+  }, []);
 
   const isForeignCurrency = () => currency !== mainCurrencyForThisMarket;
   // TODO - change it to use mixed currencies pair exchange rate
@@ -53,7 +60,10 @@ export const PricingBlock = ({
   return (
     <>
       <div className="relative col-span-full -ml-2 h-full w-screen md:col-span-1 md:-ml-0 md:w-full">
-        <div className="sticky top-[95px] h-fit overflow-hidden rounded-md shadow-lg">
+        <div
+          className="sticky h-fit overflow-hidden rounded-md shadow-lg"
+          style={{ top: headerHeight + 12 }}
+        >
           <div className="flex h-[54px] w-full items-center justify-center bg-orange">
             <IoPricetagsOutline />
             <span className="text-paragraph ml-1">{pricingBlockLabels.priceLabel}</span>
