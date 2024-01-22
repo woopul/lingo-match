9f54aabd8ba192cbbfc691e1d8ec224fc64b32c3
Maciej Opulski <maciopul@gmail.com>
2024-01-22 13:16:46 +0100

    first commit on new branch

---
+++ src/components/Organisms/AsideStickyLinksBlock/AsideStickyLinksBlock.tsx
@@ -0,0 +1,30 @@
+import { LinkIcon } from '@lingo-match/components/Atoms';
+import { PlatformListDTO } from '@lingo-match/types/strapi';
+import React from 'react';
+
+type AsideStickyLinksBlockProps = {
+  platformList?: PlatformListDTO;
+};
+
+const AsideStickyLinksBlock = ({ platformList }: AsideStickyLinksBlockProps) => {
+  return (
+    <>
+      <div className="mb-1 mt-0.5">{platformList?.title}</div>
+      <div className="flex flex-col gap-1">
+        {platformList?.links?.map((link, i) => {
+          return (
+            <LinkIcon
+              className="text-paragraph uppercase"
+              href={link.href || ''}
+              iconSrc={link.icon?.data?.attributes.url || ''}
+              key={`${link.href}-${link.label || ''}-${i}`}
+              label={link.label || ''}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default AsideStickyLinksBlock;
--- src/components/Organisms/PricingBlock/PricingBlock.tsx
+++ src/components/Organisms/PricingBlock/PricingBlock.tsx
@@ -55,75 +55,66 @@ export const PricingBlock = ({
 
   return (
     <>
-      <div className="relative col-span-full -ml-2 h-full w-screen md:col-span-1 md:-ml-0 md:w-full">
-        <div
-          className="sticky h-fit overflow-hidden rounded-md shadow-lg"
-          style={{ top: headerHeight + 12 }}
-        >
-          <div className="flex h-[54px] w-full items-center justify-center bg-orange">
-            <IoPricetagsOutline />
-            <span className="text-paragraph ml-1">{pricingBlockLabels.priceLabel}</span>
-          </div>
-          <div
-            className={cn(
-              'min-h-[80px]',
-              '[&>*:last-of-type]:border-b-0 [&>*]:border-b-[1px] [&>*]:border-lightGrey',
-            )}
-          >
-            {subscriptionType?.map((item, i) => {
-              return (
-                <div className="mx-2 border-b-[1px] py-1.5" key={i}>
-                  <h3 className="text-paragraph pb-2 text-center">
-                    {item.subscription.data?.attributes.title}
-                  </h3>
-                  <div className="text-small mt-auto flex flex-col gap-0.5 text-right">
-                    {isForeignCurrency() && pricingBlockLabels.foreignCurrencyDescriptionLabel && (
-                      <div className="text-small mt-auto">
-                        {pricingBlockLabels.foreignCurrencyDescriptionLabel}
-                      </div>
-                    )}
-                    <div className="text-small flex justify-end">
-                      {!!item.priceBeforeDiscountAsNumber && (
-                        <div className="mr-1 text-accentOne line-through">
-                          {parseAndFormatPriceToCorrectCurrency(item.priceBeforeDiscountAsNumber)}{' '}
-                          {mainCurrencyForThisMarket}
-                        </div>
-                      )}
-                      <div>
-                        <span className="text-16 font-bold text-black">
-                          {parseAndFormatPriceToCorrectCurrency(item.priceAsNumber)}{' '}
-                          {mainCurrencyForThisMarket}
-                        </span>
-                        <span className="text-middleGrey">
-                          {pricingBlockLabels.pricePerMonthLabel}
-                        </span>
-                      </div>
+      <div className="flex h-[54px] w-full items-center justify-center bg-orange">
+        <IoPricetagsOutline />
+        <span className="text-paragraph ml-1">{pricingBlockLabels.priceLabel}</span>
+      </div>
+      <div
+        className={cn(
+          'min-h-[80px]',
+          '[&>*:last-of-type]:border-b-0 [&>*]:border-b-[1px] [&>*]:border-lightGrey',
+        )}
+      >
+        {subscriptionType?.map((item, i) => {
+          return (
+            <div className="mx-2 border-b-[1px] py-1.5" key={i}>
+              <h3 className="text-paragraph pb-2 text-center">
+                {item.subscription.data?.attributes.title}
+              </h3>
+              <div className="text-small mt-auto flex flex-col gap-0.5 text-right">
+                {isForeignCurrency() && pricingBlockLabels.foreignCurrencyDescriptionLabel && (
+                  <div className="text-small mt-auto">
+                    {pricingBlockLabels.foreignCurrencyDescriptionLabel}
+                  </div>
+                )}
+                <div className="text-small flex justify-end">
+                  {!!item.priceBeforeDiscountAsNumber && (
+                    <div className="mr-1 text-accentOne line-through">
+                      {parseAndFormatPriceToCorrectCurrency(item.priceBeforeDiscountAsNumber)}{' '}
+                      {mainCurrencyForThisMarket}
                     </div>
-                    <div className="text-[11px] text-middleGrey">
-                      {pricingBlockLabels.priceForShortLabel}
-                    </div>
+                  )}
+                  <div>
+                    <span className="text-16 font-bold text-black">
+                      {parseAndFormatPriceToCorrectCurrency(item.priceAsNumber)}{' '}
+                      {mainCurrencyForThisMarket}
+                    </span>
+                    <span className="text-middleGrey">{pricingBlockLabels.pricePerMonthLabel}</span>
                   </div>
                 </div>
-              );
-            })}
-            {freeRegistration && (
-              <div className="mx-2 py-1.5 pr-1">
-                <h3 className="text-small pb-0.5 text-right font-thin !tracking-[.1em] text-middleGrey">
-                  {pricingBlockLabels.freeRegistrationTitle}
-                </h3>
-                <h4 className="text-h3 text-right !tracking-[.05em] text-accentTwo">
-                  {pricingBlockLabels.freeRegistrationAcccent}
-                </h4>
+                <div className="text-[11px] text-middleGrey">
+                  {pricingBlockLabels.priceForShortLabel}
+                </div>
               </div>
-            )}
+            </div>
+          );
+        })}
+        {freeRegistration && (
+          <div className="mx-2 py-1.5 pr-1">
+            <h3 className="text-small pb-0.5 text-right font-thin !tracking-[.1em] text-middleGrey">
+              {pricingBlockLabels.freeRegistrationTitle}
+            </h3>
+            <h4 className="text-h3 text-right !tracking-[.05em] text-accentTwo">
+              {pricingBlockLabels.freeRegistrationAcccent}
+            </h4>
           </div>
-          <CTAButton
-            className="hidden md:flex"
-            linkCTA={linkCTA}
-            navigateToCTAButtonLabel={pricingBlockLabels.navigateToCTAButtonLabel}
-          />
-        </div>
+        )}
       </div>
+      <CTAButton
+        className="hidden md:flex"
+        linkCTA={linkCTA}
+        navigateToCTAButtonLabel={pricingBlockLabels.navigateToCTAButtonLabel}
+      />
       <CTAButton
         className="sticky bottom-0 -ml-2 w-screen md:hidden"
         linkCTA={linkCTA}
